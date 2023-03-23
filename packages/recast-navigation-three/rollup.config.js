cf962af2027588d1d9b3bc7b600e2f4e2405915f
“Isaac <isaac@isaacmason.com>
2023-03-23 15:34:26 +1000

    wip - rollup instead of vite

---
+++ packages/recast-navigation-three/rollup.config.js
@@ -0,0 +1,38 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import path from 'path';
+
+import filesize from 'rollup-plugin-filesize';
+
+export default [
+  {
+    input: `./src/index.ts`,
+    output: [
+      {
+        file: `dist/index.es.js`,
+        format: 'es',
+        sourcemap: true,
+        exports: 'named',
+      },
+      {
+        file: `dist/index.cjs.js`,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      terser(),
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: path.resolve(__dirname, `tsconfig.json`),
+        sourceMap: true,
+        inlineSources: true,
+      }),
+      filesize(),
+    ],
+  },
+];
