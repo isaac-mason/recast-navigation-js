02c91312b93e008917612c48c2471eddbf0a7826
“Isaac <isaac@isaacmason.com>
2023-06-14 21:31:51 +1000

    feat(storybook): remove storysource and docs addons

--- packages/recast-navigation/.storybook/main.ts
+++ packages/recast-navigation/.storybook/main.ts
@@ -3,32 +3,13 @@ module.exports = {
     './stories/**/*.stories.mdx',
     './stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    {
-      name: '@storybook/addon-storysource',
-      options: {
-        loaderOptions: {
-          injectStoryParameters: false,
-        },
-      },
-    },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        sourceLoaderOptions: {
-          injectStoryParameters: false,
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
   staticDirs: ['./public'],
   docs: {
-    autodocs: true,
+    autodocs: false,
   },
 };
