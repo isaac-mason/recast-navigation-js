1f2bb6b178b3fed9a63817f05250b61c412cdfae
“Isaac <isaac@isaacmason.com>
2023-08-08 22:50:15 +1000

    fix: remove @ts-expect-error, three MeshoptDecoder now has type defs

--- apps/navmesh-website/src/features/upload/gltf-loader.ts
+++ apps/navmesh-website/src/features/upload/gltf-loader.ts
@@ -2,7 +2,6 @@ import { REVISION, WebGLRenderer } from 'three';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader.js';
-// @ts-expect-error missing type
 import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 
 const THREE_PATH = `https://unpkg.com/three@0.${REVISION}.x`;
