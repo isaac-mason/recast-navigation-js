bc6db28a88bfe6fde9e74304c0fc94eea66baef9
“Isaac <isaac@isaacmason.com>
2023-12-05 08:30:08 +1000

    chore: remove unused dev dep, fix dep dep warning, fix eslint configs

--- packages/recast-navigation-core/.eslintrc.cjs
+++ packages/recast-navigation-core/.eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:import/typescript',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:storybook/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
