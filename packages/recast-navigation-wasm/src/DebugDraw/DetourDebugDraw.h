38528c43491aabf8a3ca87be6c84c63d751a9db9
“Isaac <isaac@isaacmason.com>
2024-04-24 23:25:43 +1000

    feat: format files

--- packages/recast-navigation-wasm/src/DebugDraw/DetourDebugDraw.h
+++ packages/recast-navigation-wasm/src/DebugDraw/DetourDebugDraw.h
@@ -13,7 +13,6 @@ public:
     void debugDrawNavMeshPortals(struct duDebugDraw* dd, const dtNavMesh& mesh);
     void debugDrawNavMeshPolysWithFlags(struct duDebugDraw* dd, const dtNavMesh& mesh, const unsigned short polyFlags, const unsigned int col);
     void debugDrawNavMeshPoly(struct duDebugDraw* dd, const dtNavMesh& mesh, dtPolyRef ref, const unsigned int col);
-
     void debugDrawTileCacheLayerAreas(struct duDebugDraw* dd, const dtTileCacheLayer& layer, const float cs, const float ch);
     void debugDrawTileCacheLayerRegions(struct duDebugDraw* dd, const dtTileCacheLayer& layer, const float cs, const float ch);
     void debugDrawTileCacheContours(duDebugDraw* dd, const struct dtTileCacheContourSet& lcset, const float* orig, const float cs, const float ch);
--- packages/recast-navigation-wasm/src/DebugDraw/RecastDebugDraw.h
+++ packages/recast-navigation-wasm/src/DebugDraw/RecastDebugDraw.h
@@ -7,26 +7,15 @@ class RecastDebugDraw
 {
 public:
     void debugDrawHeightfieldSolid(duDebugDraw* dd, const rcHeightfield& hf);
-
     void debugDrawHeightfieldWalkable(duDebugDraw* dd, const rcHeightfield& hf);
-
     void debugDrawCompactHeightfieldSolid(duDebugDraw* dd, const rcCompactHeightfield& chf);
-
     void debugDrawCompactHeightfieldRegions(duDebugDraw* dd, const rcCompactHeightfield& chf);
-
     void debugDrawCompactHeightfieldDistance(duDebugDraw* dd, const rcCompactHeightfield& chf);
-
     void debugDrawHeightfieldLayer(duDebugDraw* dd, const rcHeightfieldLayer& layer, const int idx);
-
     void debugDrawHeightfieldLayers(duDebugDraw* dd, const rcHeightfieldLayerSet& lset);
-
     void debugDrawRegionConnections(duDebugDraw* dd, const rcContourSet& cset, const float alpha);
-
     void debugDrawRawContours(duDebugDraw* dd, const rcContourSet& cset, const float alpha);
-
     void debugDrawContours(duDebugDraw* dd, const rcContourSet& cset, const float alpha);
-
     void debugDrawPolyMesh(duDebugDraw* dd, const rcPolyMesh& mesh);
-
     void debugDrawPolyMeshDetail(duDebugDraw* dd, const rcPolyMeshDetail& dmesh);
 };
