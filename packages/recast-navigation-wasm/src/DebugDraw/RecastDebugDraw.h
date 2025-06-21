2585b7c2a25154f716a45e22cbeaa6000d82a51f
local <local@local.local>
2025-06-20 20:19:05 -0700

    expose additional debug drawing functions

--- packages/recast-navigation-wasm/src/DebugDraw/RecastDebugDraw.h
+++ packages/recast-navigation-wasm/src/DebugDraw/RecastDebugDraw.h
@@ -2,14 +2,15 @@
 
 #include "../../recastnavigation/DebugUtils/Include/DebugDraw.h"
 #include "../../recastnavigation/DebugUtils/Include/RecastDebugDraw.h"
+
 #include "../Arrays.h"
 
 class RecastDebugDraw
 {
 public:
-    void debugDrawTriMesh(duDebugDraw* dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const UnsignedCharArray *flags, const float texScale);
-    void debugDrawTriMeshNoFlags(duDebugDraw* dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const float texScale);
-    void debugDrawTriMeshSlope(duDebugDraw* dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const float walkableSlopeAngle, const float texScale);
+    void debugDrawTriMesh(duDebugDraw *dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const UnsignedCharArray *flags, const float texScale);
+    void debugDrawTriMeshNoFlags(duDebugDraw *dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const float texScale);
+    void debugDrawTriMeshSlope(duDebugDraw *dd, const FloatArray *verts, int nverts, const IntArray *tris, const FloatArray *normals, int ntris, const float walkableSlopeAngle, const float texScale);
 
     void debugDrawHeightfieldSolid(duDebugDraw *dd, const rcHeightfield &hf);
     void debugDrawHeightfieldWalkable(duDebugDraw *dd, const rcHeightfield &hf);
